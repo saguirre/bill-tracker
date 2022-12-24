5e74596085ee9e0b0b0c997b3972a267866b987c
Santi Aguirre <saguirrews@gmail.com>
2022-12-24 16:09:34 -0500

    Added password strength indicator

--- web/components/ThemeChanger.tsx
+++ web/components/ThemeChanger.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from '@heroicons/react/24/outline';
 import { useTheme } from 'next-themes';
 import { useState, useEffect } from 'react';
 
--- web/components/signup/SignUpForm.tsx
+++ web/components/signup/SignUpForm.tsx
@@ -4,6 +4,8 @@ import { BanknotesIcon } from '@heroicons/react/24/outline';
 import { emailRegex } from '../../utils/email-regex.util';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import classNames from 'classnames';
+import { useState } from 'react';
 
 interface FormValues {
   name: string;
@@ -27,6 +29,24 @@ export const SignUpForm: React.FC<SignUpFormProps> = ({ submit, loadingRequest }
     setValue,
     formState: { errors },
   } = useForm<FormValues>({ mode: 'onTouched' });
+
+  const [passwordStrength, setPasswordStrength] = useState(0);
+  const getPasswordSuccessPercentage = (password: string) => {
+    let percentage = 0;
+    if (password.length >= 6) {
+      percentage += 25;
+    }
+    if (password.match(/[a-z]/)) {
+      percentage += 25;
+    }
+    if (password.match(/[A-Z]/)) {
+      percentage += 25;
+    }
+    if (password.match(/[0-9]/)) {
+      percentage += 25;
+    }
+    return percentage;
+  };
 
   const onSubmit: SubmitHandler<FormValues> = (data: FormValues) => {
     submit(data);
@@ -91,23 +111,68 @@ export const SignUpForm: React.FC<SignUpFormProps> = ({ submit, loadingRequest }
                   })}
                   autoComplete="email"
                 />
-                <FormInput
-                  id="password"
-                  htmlFor="password"
-                  type="password"
-                  labelText="Password"
-                  errors={errors}
-                  placeholder="Password"
-                  {...register('password', {
-                    required: 'Password is required',
-                    minLength: {
-                      value: 6,
-                      message: 'Password should be at least 6 characters long',
-                    },
-                    maxLength: { value: 50, message: 'Password should not exceed 50 characters' },
-                  })}
-                  autoComplete="current-password"
-                />
+                <div>
+                  <FormInput
+                    id="password"
+                    htmlFor="password"
+                    type="password"
+                    labelText="Password"
+                    placeholder="Password"
+                    {...register('password', {
+                      required: 'Password is required',
+                      minLength: {
+                        value: 6,
+                        message: 'Password should be at least 6 characters long',
+                      },
+                      validate: (value) => {
+                        if (!value.match(/[a-z]/)) {
+                          return 'Password should contain at least one lowercase letter';
+                        } else if (!value.match(/[A-Z]/)) {
+                          return 'Password should contain at least one uppercase letter';
+                        } else if (!value.match(/[0-9]/)) {
+                          return 'Password should contain at least one number';
+                        }
+                        return true;
+                      },
+                      maxLength: { value: 50, message: 'Password should not exceed 50 characters' },
+                      onChange: (e) => {
+                        setPasswordStrength(getPasswordSuccessPercentage(watch('password')));
+                      },
+                    })}
+                    autoComplete="current-password"
+                  />
+                  <div className="w-full mt-3 flex flex-col items-center justify-center">
+                    <progress
+                      className={classNames('progress w-56', {
+                        'progress-error': passwordStrength <= 25,
+                        'progress-warning': passwordStrength > 25 && passwordStrength < 100,
+                        'progress-success': passwordStrength === 100,
+                      })}
+                      value={passwordStrength}
+                      max="100"
+                    ></progress>
+                    <div className='w-full flex items-center justify-center'>
+                      {passwordStrength <= 25 && (
+                        <>
+                          {errors.password && (
+                            <span className="text-sm text-center text-error mt-1">{errors.password.message}</span>
+                          )}
+                        </>
+                      )}
+                      {passwordStrength > 25 && passwordStrength < 100 && (
+                        <>
+                        {errors.password && (
+                          <span className="text-sm text-center text-warning mt-1">{errors.password.message}</span>
+                        )}
+                      </>
+                      )}
+                      {passwordStrength === 100 && (
+                        <span className="text-sm text-center text-success mt-1">Password is strong</span>
+                      )}
+                    </div>
+                  </div>
+                </div>
+
                 <div className="w-full">
                   <FormInput
                     htmlFor="repeatPassword"
